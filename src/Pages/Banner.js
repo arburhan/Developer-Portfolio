a5062b628d4cab4dae97c9378512d8451a9727be
arburhan <buthan190629@outlook.com>
2022-06-09 21:53:00 +0600

    add react type effect

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import bannerPhoto from '../Assests/Images/bannerPhoto.png';
+import Typed from "react-typed"
 
 const Banner = () => {
     return (
@@ -8,9 +9,18 @@ const Banner = () => {
                 <div class="hero-content flex-col lg:flex-row-reverse gap-5">
                     <img src={bannerPhoto} class="w-sm md:max-w-md rounded-lg shadow-2xl" alt='' />
                     <div className='w-xs md:max-w-4xl' >
-                        <p className="text-md md:text-xl">Hello I'm</p>
+                        <p className="text-md">Hello I'm</p>
                         <h1 className="text-2xl md:text-5xl font-bold py-1">Md Borhan Uddin Ashik</h1>
-                        <h2 className="text-xl md:text-3xl">Junior Web Developer</h2>
+                        <Typed
+                            className="text-xl md:text-3xl text-red-500"
+                            strings={[
+                                "Junior Web Developer",
+                                "Full-Stack Web Developer",
+                            ]}
+                            typeSpeed={100}
+                            backSpeed={70}
+                            loop
+                        />
                         <p className="text-md py-4 md:pr-8">I am a junior web developer. I love coding and coding is my passion. I like to constantly get acquainted with new technologies and take on challenges and I find joy in it. Recently I created some fullstack websites that you can find in the projects area.</p>
                         <div className='flex gap-5'>
                             {/* <button class="btn underline">Hire Me</button> */}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AboutMe from './AboutMe';
 import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AboutMe></AboutMe>
         </div>
     );
 };
