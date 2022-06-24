3517796f3871dd3df51ab49dd87a6637914ad439
arburhan <buthan190629@outlook.com>
2022-06-24 22:47:16 +0600

    add hire link

--- src/Pages/Banner.js
+++ src/Pages/Banner.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import bannerPhoto from '../Assests/Images/bannerPhoto.png';
 import Typed from "react-typed";
-import bannerBackgroundImage from '../Assests/Images/background-image.jpg';
+
 
 const Banner = () => {
     const handleDownloadCV = () => {
         window.open('https://drive.google.com/file/d/17DQmFH0WH_zj2VxiLZ6ma6eqCywf4-iR/view?usp=sharing');
     }
     return (
-        <div
-            style={{
-                /*  background: `url(${bannerBackgroundImage})`,
-                 backgroundSize: 'cover', */
-            }}>
+        <div style={{}}>
             <div data-aos="zoom-in" class="hero h-[85vh]">
                 <div class="hero-content flex-col lg:flex-row-reverse gap-5">
                     <img src={bannerPhoto} class="w-sm md:max-w-md rounded-lg shadow-2xl" alt='' />
@@ -32,7 +28,11 @@ const Banner = () => {
                         <p className="text-md py-4 md:pr-8">I am a junior web developer. I love coding and coding is my passion. I like to constantly get acquainted with new technologies and take on challenges and I find joy in it. Recently I created some fullstack websites that you can find in the projects area.</p>
                         <div className='flex gap-8'>
                             <button onClick={handleDownloadCV} class="btn btn-accent">Download Resume</button>
-                            <button class="underline bg-slate-700 rounded-md text-accent p-3">Hire Me</button>
+                            <button
+                                onClick={() => {
+                                    window.open('https://www.fiverr.com/burhan190629?up_rollout=true')
+                                }}
+                                class="underline bg-slate-700 rounded-md text-accent p-3">Hire Me</button>
                         </div>
                     </div>
                 </div>
