ebf3a343146e3517a493ce6e5db4227e399ee8ef
Md Borhan Uddin Ashik <90565640+arburhan@users.noreply.github.com>
2023-10-12 20:10:09 +0600

    Update ContactMe.js

--- src/Pages/ContactMe.js
+++ src/Pages/ContactMe.js
@@ -39,7 +39,7 @@ const ContactMe = () => {
                                 <p className='underline-offset-2'>LinkedIn</p>
                             </div>
                             <div onClick={() => {
-                                window.open('https://www.facebook.com/ar.burhan.560/')
+                                window.open('https://www.facebook.com/arBurhann/')
                             }} className="flex flex-col items-center hover:underline  cursor-pointer bg-slate-600 rounded-2xl py-4 px-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z" /></svg>
                                 <p className='underline-offset-2'>Facebook</p>
